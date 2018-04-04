0fdeaf9d61004b8cf206b36184e8bd02ab7bdbe9
root <sbuckley12489@gmail.com>
2018-04-04 00:30:27 +0000

    Counts unique total devices and sends information to server.  TODO: Add signal handler for thread to kill UpdateServer thread after keyboard interrupt. TODO: determine how to remove devices that havne't been seen in a while

--- sniff.py
+++ sniff.py
@@ -1,12 +1,31 @@
 from scapy.all import *
+import requests
+import threading
+import time
+
+#modify the below URL to contact our server
+url = 'http://a70f6c06.ngrok.io/18748.php'
+
+
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
+
+macDict = {}
+
+def UpdateServer():
+    print("Sending message to server with: %d value"%len(macDict))
+    requests.post(url, data={"id":5, "num_people":len(macDict)})
+    t = threading.Timer(10.0, UpdateServer)
+    t.start()
 
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
         if pkt.type==PROBE_REQUEST_TYPE and pkt.subtype==PROBE_REQUEST_SUBTYPE:
             PrintPacket(pkt)
+            if(pkt.addr2 not in macDict):
+                macDict[pkt.addr2] = 1
+
 
 def PrintPacket(pkt):
     try:
@@ -20,5 +39,9 @@ def PrintPacket(pkt):
         print("No signal strength found")
     print "Target: %s Source: %s SSID: %s RSSI: %d"%(pkt.addr3, pkt.addr2, pkt.getlayer(Dot11ProbeReq).info, signal_strength)
 
+#TODO: Change time value back to 30 or 60. 10 is for testing purposes
+#t = threading.Timer(10.0, UpdateServer)
+#t.start()
+UpdateServer()
 sniff(iface="ra0", prn=PacketHandler)
 
